f200868972bbb9bb7d543a1ec7051d1f561dea91
Rudra-solanki <157590645+Solanki-rudra@users.noreply.github.com>
2025-07-23 22:46:09 +0530

    basic file adding setup

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
 
 app.set('view engine', 'ejs');
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
-    res.render('index');
+    fs.readdir('./files', (err, files) => {
+        res.render('index', { files });
+    })
+})
+
+app.post('/create', (req, res) => {
+    const data = req.body
+    fs.writeFile(`./files/${data.title.replaceAll(" ", "")}.txt`, data.description, (err) => {
+        if (err) {
+            console.log(err.message);
+        }
+        res.redirect('/');
+    })
 })
 
 app.listen(3000, () => {
